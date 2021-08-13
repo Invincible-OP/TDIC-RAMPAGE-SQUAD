f9fc32c88734e0baaab66647d1d4a4eb61c6e3c3
AditSarda <81905978+AditSarda@users.noreply.github.com>
2021-08-13 12:43:15 +0530

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,35 @@
+var aboutus = document.getElementById("aboutus");
+
+var member = document.getElementById("member");
+
+var rank = document.getElementById("rank");
+
+var sports = document.getElementById("sports");
+
+var home = document.getElementById("home");
+
+var whatsapp = document.getElementById("whatsapp");
+
+aboutus.onclick = () => {
+    location.href = "./aboutus.html";
+}
+
+member.onclick = () => {
+    location.href = "./member.html";
+}
+
+rank.onclick = () => {
+    location.href = "./rank.html";
+}
+
+sports.onclick = () => {
+    location.href = "./sports.html";
+}
+
+home.onclick = () => {
+    location.href = "./index.html";
+}
+
+whatsapp.onclick = () => {
+    location = "https://chat.whatsapp.com/Etf4BnuazkN1YkrE3dvhvb";
+}
